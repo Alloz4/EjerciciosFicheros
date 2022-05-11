6ef747a4cabc9ed03438ede423fcec1e09809e2c
Marcos <marcos.alloza4@gmail.com>
2022-05-11 13:23:37 +0200

    Ejercicio 2 terminado

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,3 +1,6 @@
+/*
+ * Realizar un comando que muestre un fichero de texto con sus líneas invertidas.
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -6,13 +9,22 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		File archivo = new File("C:/Users/34670/Desktop/JAVA eclipse/Ficheros-ejercicios/Ficheros/ejericicio2.txt");
+		File archivo = new File("C:/Users/34670/Desktop/JAVA eclipse/Ficheros-ejercicios/Ficheros/ejercicio2.txt");
 		
 		Scanner sc = new Scanner(archivo);
 		
-		
-		
-		
+		String linea = null;
+
+		while (sc.hasNextLine()) {
+			linea = sc.nextLine();
+			String alreves = "";
+			
+			for(int i = linea.length() - 1; i >= 0; i--) {
+				alreves += linea.charAt(i) ;
+			}
+			System.out.println(alreves);
+		}		
+		sc.close();
 	}
 
 }
