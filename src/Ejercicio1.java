26b091db7159d95cf1e0a279be12b13b84a37415
Marcos <marcos.alloza4@gmail.com>
2022-05-11 11:49:17 +0200

    Ejercicio 1 terminado

---
+++ src/Ejercicio1.java
@@ -0,0 +1,31 @@
+/*
+ * Realizar un comando en java para visualizar ficheros de texto que me permita mostrar el fichero pantalla a pantalla (24 líneas consecutivas), realizando una espera después de cada pantalla hasta que el usuario pulse Intro, para mostrar la siguiente página del fichero.
+ */
+import java.io.*;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File archivo = new File("C:/Users/34670/Desktop/fichero.txt");
+		
+		Scanner sc = new Scanner(archivo);
+		Scanner salto = new Scanner(System.in);
+		
+		String linea;
+		String intro;
+		int contador = 0;
+		while (sc.hasNext()) {
+			linea = sc.nextLine() ;
+			System.out.print(linea);
+			intro = salto.nextLine();
+			if (contador == 23) {
+				break;
+			}
+			contador++;
+		}
+		
+	}
+
+}
