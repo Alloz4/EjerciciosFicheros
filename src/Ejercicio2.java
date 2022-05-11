eac5638f33f3b979fe8c1d81986b45a57a7299a7
Marcos <marcos.alloza4@gmail.com>
2022-05-11 12:20:59 +0200

    Añadiendo el ejercicio 2 al repositorio

---
+++ src/Ejercicio2.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File archivo = new File("C:/Users/34670/Desktop/JAVA eclipse/Ficheros-ejercicios/Ficheros/ejericicio2.txt");
+		
+		Scanner sc = new Scanner(archivo);
+		
+		
+		
+		
+	}
+
+}
